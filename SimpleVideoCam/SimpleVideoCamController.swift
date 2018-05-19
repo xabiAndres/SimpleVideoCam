9a8cd6734822ecf71b52ed32fbacd834e62eac0f
Xabier Andres <xabier_andres@hotmail.com>
2018-05-19 10:24:25 +0200

    Initial Commit

---
+++ SimpleVideoCam/SimpleVideoCamController.swift
@@ -0,0 +1,116 @@
+//
+//  SimpleVideoCamController.swift
+//  SimpleVideoCam
+//
+//  Created by Simon Ng on 17/10/2016.
+//  Copyright © 2016 AppCoda. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import AVKit
+
+class SimpleVideoCamController: UIViewController {
+
+    let captureSession = AVCaptureSession()
+    var currentDevice: AVCaptureDevice!
+    var videoFileOutput: AVCaptureMovieFileOutput!
+    var cameraPreviewLayer: AVCaptureVideoPreviewLayer?
+    var isRecording = false
+    
+    @IBOutlet var cameraButton:UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configure()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    private func configure() {
+        // Preset the session for taking photo in full resolution
+        captureSession.sessionPreset = AVCaptureSession.Preset.high
+        
+        // Get the back-facing camera for capturing videos
+        let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInDualCamera], mediaType: AVMediaType.video, position: .back)
+        
+        guard let device = deviceDiscoverySession.devices.first else {
+            print("Failed to get the camera device.")
+            return
+        }
+        
+        currentDevice = device
+        
+        // Get the input data source
+        guard let captureDeviceInput = try? AVCaptureDeviceInput(device: currentDevice) else { return }
+        
+        // Configure the session with output for capturing video
+        videoFileOutput = AVCaptureMovieFileOutput()
+        
+        // Configure the session with the input and output devices
+        captureSession.addInput(captureDeviceInput)
+        captureSession.addOutput(videoFileOutput)
+        
+        // Provide a camera preview
+        cameraPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        view.layer.addSublayer(cameraPreviewLayer!)
+        cameraPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        cameraPreviewLayer?.frame = view.layer.frame
+        
+        // Bring the camera button to front
+        view.bringSubview(toFront: cameraButton)
+        captureSession.startRunning()
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "playVideo" {
+            let videoPlayerViewController = segue.destination as! AVPlayerViewController
+            let videoFileURL = sender as! URL
+            videoPlayerViewController.player = AVPlayer(url: videoFileURL)
+        }
+    }
+    
+    // MARK: - Action methods
+    
+    @IBAction func unwindToCamera(segue:UIStoryboardSegue) {
+        
+    }
+    
+    @IBAction func capture(sender: AnyObject) {
+        if !isRecording {
+            isRecording = true
+            
+            UIView.animate(withDuration: 0.5, delay: 0.0, options: [.repeat, .autoreverse, .allowUserInteraction], animations: {
+                self.cameraButton.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+            }, completion: nil)
+            
+            let outputPath = NSTemporaryDirectory() + "output.mov"
+            let outputFileURL = URL(fileURLWithPath: outputPath)
+            videoFileOutput?.startRecording(to: outputFileURL, recordingDelegate: self)
+        } else {
+            isRecording = false
+            
+            UIView.animate(withDuration: 0.5, delay: 0.0, options: [], animations: {
+                self.cameraButton.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+            }, completion: nil)
+            cameraButton.layer.removeAllAnimations()
+            videoFileOutput?.stopRecording()
+        }
+    }
+
+}
+
+extension SimpleVideoCamController: AVCaptureFileOutputRecordingDelegate {
+    func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
+        guard error == nil else {
+            print(error ?? "")
+            return
+        }
+        
+        performSegue(withIdentifier: "playVideo", sender: outputFileURL)
+    }
+}
